4aef274cf5ce3b822da316b3bc5f6b9a849ca794
watcharapol yotadee <watcharapolyotadee@outlook.com>
2020-08-15 14:07:39 +0700

    Succeed

--- Even_Fibonacci_Numbers_WeekOne/Source.cpp
+++ Even_Fibonacci_Numbers_WeekOne/Source.cpp
@@ -3,7 +3,7 @@ int main()
 {
 	int num1 = 0, num2 = 0, sum_num12 = 0, sumAns = 0;
 	num1 = num2 = sum_num12 = 1;
-	printf("%d\t%d\t%d\n", num1, num2, sum_num12);
+	printf("num1 = %d\tnum2 = %d\tsum_num12 = %d\n", num1, num2, sum_num12);
 	for (; sum_num12 <= 4000000; sum_num12 = num1 + num2)
 	{
 		if (sum_num12 % 2 == 0)
@@ -11,7 +11,11 @@ int main()
 			sumAns += sum_num12;
 
 		}
-		
+		num1 = num2;
+		printf("\nnum1 = %d \t num2 = %d\n ", num1, num2);
+		num2 = sum_num12;
+		printf("\nnum2 = %d \t sum_num12 = %d \n ", num2, sum_num12);
+		printf("\nsumAns = %d \n", sumAns);
 
 
 	}
